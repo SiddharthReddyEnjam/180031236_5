f776300599ec25ae2096b7a3104ff22adfd6d9ee
Siddharth <Siddharth@Siddharth>
2021-05-12 12:42:29 +0530

    Command Line arguments

---
+++ Command Line Arguments/src/Accepttwostrings.java
@@ -0,0 +1,8 @@
+class Accepttwostrings 
+{
+public static void main(String args[]) 
+{
+		// TODO Auto-generated method stub
+	System.out.println(args[0]+" Technologies "+args[1]);
+}
+}
---
+++ Command Line Arguments/src/Welcome.java
@@ -0,0 +1,7 @@
+class Welcome {
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Welcome"+args[0]);
+	}
+}
